65c7a3e10512eca21273098401d3a90e13f4918b
bekatadesse <bekatadesse5@gmail.com>
2022-11-30 11:45:02 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,13 +8,14 @@ def number_of_subscribers(subreddit):
     """ this functiion returns the number of subscriber
     """
     endpoint = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {"User-Agent": "user one (by u/Known_Comb558)"}
+    headers = {"User-Agent": "user one (by u/Known_Comb558)"}    
     params = {
         "limit": 10
     }
-    response = requests.get(endpoint, headers=headers, params=params)
-    if response.status_code != 200:
-        return 0
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
     results = response.json().get("data")
     [print(r.get("data").get("title")) for r in results.get("children")]
-    return response.json()['data']
