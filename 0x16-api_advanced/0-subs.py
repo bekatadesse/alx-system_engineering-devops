bacdf145fd7be6b957487311074e071a0d952b68
bekatadesse <bekatadesse5@gmail.com>
2022-11-30 11:08:44 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Function to query subscribers on a given Reddit subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """ this functiion returns the number of subscriber
-    <platform>:<app ID>:<version string> (by /u/<reddit username>)
     """
-    endpoint = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "user one (by u/Known_Comb558)"
-    }
+    endpoint = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {"User-Agent": "user one (by u/Known_Comb558)"}
     response = requests.get(endpoint, headers=headers)
     if response.status_code != 200:
         return 0
