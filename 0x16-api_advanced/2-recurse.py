8f6ac7dced5b98466e28bbdb18926c2783f60398
bekatadesse <bekatadesse5@gmail.com>
2022-11-30 15:54:47 +0300

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+""" Function to query subscribers on a given Reddit subreddit """
 import requests
 
-
-def number_of_subscribers(subreddit):
-    """ this functiion returns the number of subscriber
-    <platform>:<app ID>:<version string> (by /u/<reddit username>)
-    """
-    endpoint = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "user one (by u/Known_Comb558)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(endpoint, headers=headers, params=params)
-    if response.status_code != 200:
-        return 0
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+def recurse(subreddit, hotlist=[], after=None):
+    """ this functiion returns the number of subscriber """
+    head = {'User-Agent': "user one (by u/Known_Comb558)"}
+    try:
+        if after:
+            count = requests.get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
+                subreddit, after), headers=head).json().get('data')
+        else:
+            count = requests.get('https://www/reddit.com/r/{}/hot.json'.format(
+                subreddit), headers=head).json().get('data')
+        hotlist += [dic.get('data').get('title')
+                    for dic in count.get('children')]
+        if count.get('after'):
+            return recurse(subreddit, hotlist, after=count.get('after'))
+        return hotlist
+    except:
+        return None
