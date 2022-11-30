b9890a9739ac56ed2667054da5657288bf2009ec
bekatadesse <bekatadesse5@gmail.com>
2022-11-30 11:51:16 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,8 +12,7 @@ def top_ten(subreddit):
     params = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(endpoint, headers=headers, params=params)
     if response.status_code == 404:
         print("None")
         return
